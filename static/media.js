aa69318f3abc214f7f0f0311c83cb370535f4243
Mark Banner <mbanner@mozilla.com>
2012-12-06 11:31:18 +0000

    Actually add media.js

---
+++ static/media.js
@@ -0,0 +1,41 @@
+function getAudioVideo(aWin, aPC, aSuccessCallback, aCanFake) {
+  try {
+    getVideo(aWin, function(stream) {
+      var video = aWin.document.getElementById("localVideo");
+      video.mozSrcObject = stream;
+      video.play();
+      aPC.addStream(stream);
+      getAudio(aWin, function(stream) {
+        var audio = aWin.document.getElementById("localAudio");
+        audio.mozSrcObject = stream;
+        audio.play();
+        aPC.addStream(stream);
+        aSuccessCallback();
+      }, function(err) { alert("failed to get microphone: " + err); }, true);
+    }, function(err) { alert("failed to get camera: " + err); }, true);
+  } catch(e) { alert(e); }
+}
+
+function getVideo(aWin, aSuccessCallback, aErrorCallback, aCanFake) {
+  aWin.navigator.mozGetUserMedia({video: true}, function(stream) {
+    aSuccessCallback(stream);
+  }, function(err) {
+    if (aCanFake && err == "HARDWARE_UNAVAILABLE") {
+      aWin.navigator.mozGetUserMedia({video: true, fake: true}, aSuccessCallback, aErrorCallback);
+    } else {
+      aErrorCallback(err);
+    }
+  });
+}
+
+function getAudio(aWin, aSuccessCallback, aErrorCallback, aCanFake) {
+  aWin.navigator.mozGetUserMedia({audio: true}, function(stream) {
+    aSuccessCallback(stream);
+  }, function(err) {
+    if (aCanFake && err == "HARDWARE_UNAVAILABLE") {
+      aWin.navigator.mozGetUserMedia({audio: true, fake: true}, aSuccessCallback, aErrorCallback);
+    } else {
+      aErrorCallback(err);
+    }
+  });
+}

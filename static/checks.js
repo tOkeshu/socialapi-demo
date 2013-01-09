93f76c73bd8d1c77b010580e22448d1b4e967397
Mark Banner <mbanner@mozilla.com>
2013-01-09 22:53:04 +0000

    Complete the initial index.html checks for testing if someone has the right version of FF or not.

--- static/checks.js
+++ static/checks.js
@@ -1,7 +1,12 @@
 var webrtcChecks = {
   checkSupport: function webrtc_checkSupport(aDisplaySuccess) {
+    if (!navigator.mozGetUserMedia || !navigator.id) {
+      this.displayWarning();
+      return;
+    }
+
     try {
-      let dummy = new window.mozRTCPeerConnection()
+      var dummy = new window.mozRTCPeerConnection()
     } catch (x) {
       this.displayWarning();
       return;
@@ -12,20 +17,18 @@ var webrtcChecks = {
   },
 
   displayWarning: function webrtc_displayWarning() {
-      document.getElementById("supportwarning").removeAttribute("hidden");
-/*
-    $("#supportwarning").text('<p>To run this demo, please first install and latest <a href="https://nightly.mozilla.org/">Nightly</a> version of Mozilla Firefox.</p>' +
+    $("#supportwarning").append('<p>To run this demo, please first install and latest <a href="https://nightly.mozilla.org/">Nightly</a> version of Mozilla Firefox.</p>' +
       '<p>Then install <a href="webrtc.xpi">this add-on</a>. You may need to restart for the social api update to take full effect.</p>');
-*/
+
   },
 
   displaySuccess: function webrtc_displaySuccess() {
-      document.getElementById("supportwarning").setAttribute("hidden", true);
-/*    $("#supportwarning").text('<p>Congratulations, you have the right support to run this demo. You can either:</p>' +
+    $("#supportwarning").append('<p>Congratulations, you appear to have the right support to run this demo. You can either:</p>' +
       '<ul><li>Visit the <a href="mobile.html">Mobile test page</a></li>' +
       '<li>or install <a href="webrtc.xpi">this add-on</a> to access it via the SocialAPI.</li>' +
       '<ul><li><i>Note: you may need to restart after installing the add-on for the social API to fully start</i></li></ul>' +
-      '</ul>');
-*/
+      '</ul>' +
+      '<p>If you get issues, please try running the latest <a href="https://nightly.mozilla.org/">nightly of Firefox</p>');
+
   }
 }
